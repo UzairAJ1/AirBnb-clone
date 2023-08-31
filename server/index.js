e4018fcfbad2d6414f74b3d8b5fa2fe16d33be14
Uzair Aj <uzair.aaj@gmail.com>
2023-09-01 04:04:57 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cookieParser = require('cookie-parser')
 //const upload = multer({ dest: 'uploads/' });
 
 require('dotenv').config()
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect('mongodb+srv://uzair:123@cluster0.k2yc4jj.mongodb.net/');
 const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 //app.use('/uploads', express.static('uploads'));
